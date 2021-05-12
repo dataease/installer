95ee46abc541a6f7fca82a550f6de3340774672d
taojinlong <jinlong@fit2cloud.com>
2021-05-12 11:35:00 +0800

    feat: dataease 打包

--- dataease/install.sh
+++ dataease/install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+MYSQL_HOST=mysql
+MYSQL_SCHEMA=dataease
+MYSQL_USER=root
+MYSQL_PASSWD=Password123@mysql
+
+read -p "Use the built in database, external database supports MySQL only: (y/n)"  build_in_database
+
+if [[ -z "${build_in_database}" || "${build_in_database}" == "y"  ]];then
+ build_in_database=y
+else
+ build_in_database=n
+fi
+if [[ "${build_in_database}" == "n"   ]];then
+  read -p "Mysql database address: "  MYSQL_HOST
+  read -p "Mysql database schema: "  MYSQL_SCHEMA
+  read -p "Mysql database user: "  MYSQL_USER
+  read -p "Mysql database password: "  MYSQL_PASSWD
+fi
+
 CURRENT_DIR=$(
    cd "$(dirname "$0")"
    pwd
@@ -12,7 +31,6 @@ function log() {
 args=$@
 
 docker_config_folder="/etc/docker"
-
 compose_files="-f docker-compose.yml"
 
 DE_RUN_BASE=/opt/dataease
@@ -23,9 +41,17 @@ cp -r ./dataease/* ${DE_RUN_BASE}/
 conf_folder=${DE_RUN_BASE}/conf
 mkdir -p $conf_folder
 templates_folder=${DE_RUN_BASE}/templates
-cp -r $templates_folder/* $conf_folder
-cd $templates_folder
 
+sed -i -e "s/MYSQL_HOST/${MYSQL_HOST}/g" $templates_folder/dataease.properties
+sed -i -e "s/MYSQL_SCHEMA/${MYSQL_SCHEMA}/g" $templates_folder/dataease.properties
+sed -i -e "s/MYSQL_USER/${MYSQL_USER}/g" $templates_folder/dataease.properties
+sed -i -e "s/MYSQL_PASSWD/${MYSQL_PASSWD}/g" $templates_folder/dataease.properties
+sed -i -e "s/MYSQL_PASSWD/${MYSQL_PASSWD}/g" $templates_folder/mysql.env
+
+cp -r $templates_folder/* $conf_folder
+cp -r $templates_folder/.kettle $conf_folder
+
+cd $templates_folder
 conf_template_files=( dataease.properties be.conf de.conf)
 for i in ${conf_template_files[@]}; do
    if [ -f $i ]; then
@@ -36,7 +62,6 @@ done
 envsubst < ${CURRENT_DIR}/dataease/bin/mysql/init.sql > ${DE_RUN_BASE}/bin/mysql/init.sql
 
 cd ${CURRENT_DIR}
-mkdir -p ${DE_RUN_BASE}/data/mysql
 mkdir -p ${DE_RUN_BASE}/data/kettle
 mkdir -p ${DE_RUN_BASE}/data/fe
 mkdir -p ${DE_RUN_BASE}/data/be
@@ -64,7 +89,7 @@ else
       chmod +x /usr/bin/docker*
       chmod 754 /etc/systemd/system/docker.service
       log "... 启动 docker"
-      service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
+      systemctl daemon-reload; service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
 
    else
       log "... 在线安装 docker"
@@ -77,6 +102,7 @@ else
    if [ ! -d "$docker_config_folder" ];then
       mkdir -p "$docker_config_folder"
    fi
+fi
 
 ##Install Latest Stable Docker Compose Release
 if which docker-compose >/dev/null; then
@@ -95,8 +121,6 @@ else
    fi
 fi
 
-cd ${DE_RUN_BASE}
-
 export COMPOSE_HTTP_TIMEOUT=180
 cd ${CURRENT_DIR}
 # 加载镜像
@@ -112,7 +136,7 @@ else
 fi
 
 log "配置 dataease Service"
-\cp ${DE_RUN_BASE}/bin/dataease/dataease.service /etc/init.d/dataease
+cp ${DE_RUN_BASE}/bin/dataease/dataease.service /etc/init.d/dataease
 chmod a+x /etc/init.d/dataease
 chkconfig --add dataease
 dataeaseService=`grep "service dataease start" /etc/rc.d/rc.local | wc -l`
@@ -136,7 +160,6 @@ fi
 if which firewall-cmd >/dev/null; then
    log "防火墙端口开放"
    firewall-cmd --zone=public --add-port=80/tcp --permanent
-   firewall-cmd --zone=public --add-port=6600-6620/tcp --permanent
    firewall-cmd --reload
 fi
 
@@ -149,3 +172,32 @@ dectl status 2>&1 | tee -a ${CURRENT_DIR}/install.log
 echo -e "======================= 安装完成 =======================\n" 2>&1 | tee -a ${CURRENT_DIR}/install.log
 echo -e "请通过以下方式访问:\n URL: http://\$LOCAL_IP\n 用户名: admin\n 初始密码: dataease" 2>&1 | tee -a ${CURRENT_DIR}/install.log
 echo -e "您可以使用命令 'dectl status' 检查服务运行情况.\n" 2>&1 | tee -a ${CURRENT_DIR}/install.log-a ${CURRENT_DIR}/install.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
