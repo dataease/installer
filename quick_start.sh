9f1bf311c37f42f735bbad228232f9941a505492
xuwei-fit2cloud <wei@fit2cloud.com>
2021-07-15 22:19:43 +0800

    refactor: 重构安装脚本，增加配置文件支持

--- quick_start.sh
+++ quick_start.sh
@@ -3,6 +3,11 @@
 DEVERSION=$(curl -s https://github.com/dataease/dataease/releases/latest/download 2>&1 | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+.*(?=")')
 
 wget --no-check-certificate https://github.com/dataease/dataease/releases/latest/download/dataease-release-${DEVERSION}.tar.gz
+
+if [ ! -f dataease-release-${DEVERSION}.tar.gz ];then
+	echo "下载在线安装包失败，请试试重新执行一次安装命令。"
+	exit 1
+fi
 tar zxvf dataease-release-${DEVERSION}.tar.gz
 cd dataease-release-${DEVERSION}
 
