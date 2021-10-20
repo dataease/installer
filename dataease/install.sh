229450124469a4148c9de3d025b9b233ae21957f
xuwei-fit2cloud <wei@fit2cloud.com>
2021-10-20 11:40:47 +0800

    refactor: 优化安装脚本

--- dataease/install.sh
+++ dataease/install.sh
@@ -128,8 +128,7 @@ if [ $? -ne 0 ]; then
       chmod +x /usr/bin/docker-compose
    else
       log "... 在线安装 docker-compose"
-      COMPOSEVERSION=$(curl -s https://github.com/docker/compose/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
-      curl -L "https://github.com/docker/compose/releases/download/$COMPOSEVERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 2>&1 | tee -a ${CURRENT_DIR}/install.log
+      curl -L https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 2>&1 | tee -a ${CURRENT_DIR}/install.log
       chmod +x /usr/local/bin/docker-compose
       ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
    fi
--- quick_start.sh
+++ quick_start.sh
@@ -1,14 +1,51 @@
 #Install Latest Stable DataEase Release
 
-DEVERSION=$(curl -s https://github.com/dataease/dataease/releases/latest/download 2>&1 | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+.*(?=")')
+git_urls=('github.com' 'hub.fastgit.org')
 
-wget --no-check-certificate https://github.com/dataease/dataease/releases/latest/download/dataease-${DEVERSION}-online.tar.gz
+for git_url in ${git_urls[*]}
+do
+	success="true"
+	for i in {1..3}
+	do
+		echo -ne "检测 ${git_url} ... ${i} "
+	    curl -m 5 -kIs https://${git_url} >/dev/null
+		if [ $? != 0 ];then
+			echo "failed"
+			success="false"
+			break
+		else
+			echo "ok"
+		fi
+	done
+	if [ ${success} == "true" ];then
+		server_url=${git_url}
+		break
+	fi
+done
+
+if [ 'x${server_url}' == 'x' ];then
+    echo "没有找到稳定的下载服务器，请稍候重试"
+    exit 1
+fi
+
+
+echo "使用下载服务器 ${server_url}"
+
+DEVERSION=$(curl -s https://${server_url}/dataease/dataease/releases/latest/download 2>&1 | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+.*(?=")')
+
+curl -LOk https://${server_url}/dataease/dataease/releases/latest/download/dataease-${DEVERSION}-online.tar.gz
 
 if [ ! -f dataease-${DEVERSION}-online.tar.gz ];then
 	echo "下载在线安装包失败，请试试重新执行一次安装命令。"
 	exit 1
 fi
+
 tar zxvf dataease-${DEVERSION}-online.tar.gz
+if [ $? != 0 ];then
+	echo "下载在线安装包失败，请试试重新执行一次安装命令。"
+	rm -f dataease-${DEVERSION}-online.tar.gz
+	exit 1
+fi
 cd dataease-${DEVERSION}-online
 
 /bin/bash install.sh
