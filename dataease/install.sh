88b5a337de51339d9a51feb8a6db428b2e35713a
taojinlong <jinlong@fit2cloud.com>
2022-05-24 21:06:03 +0800

    feat: 增加自定义驱动挂在点

--- dataease/install.sh
+++ dataease/install.sh
@@ -121,6 +121,7 @@ mkdir -p ${DE_RUN_BASE}/data/fe
 mkdir -p ${DE_RUN_BASE}/data/be
 mkdir -p ${DE_RUN_BASE}/data/mysql
 mkdir -p ${DE_RUN_BASE}/data/static-resource
+mkdir -p ${DE_RUN_BASE}/custom-drivers
 
 DE_MYSQL_HOST_ORIGIN=$DE_MYSQL_HOST
 DE_MYSQL_PORT_ORIGIN=$DE_MYSQL_PORT
